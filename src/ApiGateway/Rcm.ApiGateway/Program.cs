e8aa80a0e0bfe2d3bb1d47f45c0785a3f3169369
terxc <al.stigi@yandex.ru>
2023-02-18 18:07:06 +0300

    Gateway updated to net7

--- src/ApiGateway/Rcm.ApiGateway/Program.cs
+++ src/ApiGateway/Rcm.ApiGateway/Program.cs
@@ -1,26 +1,18 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Genl.Framework;
 
-namespace Rcm.ApiGateway
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+builder.Configuration.AddJsonFile("yarp.json", false);
+
+builder.Services.AddGenl();
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetRequiredSection("reverseProxy"));
+
+var app = builder.Build();
+
+app.MapGet("/", (AppOptions options) => options.AppInfo);
+
+app.UseRouting();
+app.MapReverseProxy();
+
+app.Run();
--- src/Services/Users/Rcm.Services.Users.Api/Program.cs
+++ src/Services/Users/Rcm.Services.Users.Api/Program.cs
@@ -12,9 +12,9 @@ var app = builder.Build();
 app.UseCore();
 
 app.MapGet("/", (AppOptions options) => options.AppInfo);
-app.MediatePost<SignUpCommand>("api/account/sign-up", StatusCodes.Status204NoContent);
-app.MediatePost<SignInCommand>("api/account/sign-in");
-app.MediateGet<GetUserQuery>("api/users/{id}", policyNames: new[] { "UsersView" });
-app.MediatePut<UpdateUserCommand>("api/users/{id}", policyNames: new[] { "UsersEdit" });
+app.MediatePost<SignUpCommand>("sign-up", StatusCodes.Status204NoContent);
+app.MediatePost<SignInCommand>("sign-in");
+app.MediateGet<GetUserQuery>("users/{id}", policyNames: new[] { "UsersView" });
+app.MediatePut<UpdateUserCommand>("users/{id}", policyNames: new[] { "UsersEdit" });
 
 app.Run();
